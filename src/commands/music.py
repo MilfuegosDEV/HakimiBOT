68a927caca3ed1478f0b4a126b7f6f3cb6f06327
MilfuegosDEV <milfuegosdev@gmail.com>
2023-08-23 02:52:08 -0600

    docstrings

--- src/commands/music.py
+++ src/commands/music.py
@@ -3,18 +3,32 @@ from discord.commands import option
 from Connection import GeniusConnect, SpotiConnect
 from messages import EmbedConstructor
 
-
 class MusicCog(discord.Cog):
+    """
+    Cog class for music-related commands in a Discord bot.
+    """
 
     __MusicCommands: discord.SlashCommandGroup = discord.SlashCommandGroup(
         "music", "music commands")
 
     def __init__(self, spotifyApi: dict, geniusApi: dict) -> None:
+        """
+        Initialize the MusicCog.
+
+        :param spotifyApi: Dictionary containing Spotify API credentials.
+        :param geniusApi: Dictionary containing Genius API credentials.
+        """
         super().__init__()
         self.__spotify = SpotiConnect(ClientCredentials=spotifyApi)
         self.__genius = GeniusConnect(Credentials=geniusApi)
 
     def searchTrackByName(self, q: str):
+        """
+        Search for a track by its name using Spotify API.
+
+        :param q: Query string for track search.
+        :return: Track information if found, else None.
+        """
         results = self.__spotify.search(q=q, type="track")
         if results['tracks']['items']:
             return results['tracks']['items'][0]
@@ -22,12 +36,17 @@ class MusicCog(discord.Cog):
             return None
 
     def __get_lyrics(self, artist, track_name):
+        """
+        Get lyrics of a song using Genius API.
+
+        :param artist: Artist's name.
+        :param track_name: Track's name.
+        :return: Lyrics of the song, or "Lyrics not found" if not available.
+        """
         song = self.__genius.search_song(track_name, artist)
-
-        lyrics = song.lyrics.splitlines()
-        lyrics.pop(0)
-
         if song:
+            lyrics = song.lyrics.splitlines()
+            lyrics.pop(0)
             return "\n".join(lyrics)
         else:
             return "Lyrics not found"
@@ -35,6 +54,12 @@ class MusicCog(discord.Cog):
     @__MusicCommands.command(description="Search the song's lyrics")
     @option(name="query", type=str, description="Song's name or URL")
     async def lyrics(self, ctx: discord.context.ApplicationContext, query: str):
+        """
+        Command to search and display lyrics of a song.
+
+        :param ctx: Context of the command.
+        :param query: Song's name or URL.
+        """
         embedInfo = {
             "title": 'Song not found',
             "description": '{} not found'.format(query),
